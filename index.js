f33a05c9eb0e95c59f6101703eac1108ee06ba70
Abu Sufian <asufian47@gmail.com>
2021-01-06 13:32:46 +0900

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import I18n from './vendor/i18n';
+import { Localization } from 'expo'
+
+
+I18n.initAsync = async () => {
+    const locale = await Localization.locale();
+    I18n.locale = (locale) ? locale.replace(/_/, '-') : '';
+}
+
+export default I18n;
