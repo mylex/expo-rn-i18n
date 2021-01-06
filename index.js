b9bcd518ab0f222754f7fc186f1fc560f81dcf4f
Abu Sufian <asufian47@gmail.com>
2021-01-06 13:35:58 +0900

    Fix issue for expo sdk 35 & upper

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import I18n from './vendor/i18n';
-import { Localization } from 'expo'
+import * as Localization from 'expo-localization';
 
+/**
+ * import { Localization } from 'expo'  depricated from sdk 35
+ * Upgrade to 
+ * import * as Localization from 'expo-localization';
+ * 
+ * Use const { locale } = await Localization.getLocalizationAsync();instead of const locale = await Localization.locale(); 
+ */
 
 I18n.initAsync = async () => {
-    const locale = await Localization.locale();
+    const { locale } = await Localization.getLocalizationAsync();
     I18n.locale = (locale) ? locale.replace(/_/, '-') : '';
 }
 
